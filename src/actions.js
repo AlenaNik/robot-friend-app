57235f091e0362126226e3c1bbd5996c0b68be1d
AlenaNik <alena.nik.digital@gmail.com>
2019-05-21 19:40:26 +0200

    redux setup

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,6 @@
+import { CHANGE_SEARCHFIELD } from "./constants";
+
 export const setSearchField = (text) => ({
     type: 'CHANGE_SEARCH_FIELD',
     payload: text
-})
+});
---
+++ src/reducers.js
@@ -0,0 +1,13 @@
+import { CHANGE_SEARCHFIELD } from "./constants";
+
+const initialState  = {
+    searchField: ''
+}
+
+
+export const searchRobots = (state=initialState, action={}) => {
+        switch(action.type) {
+            case CHANGE_SEARCHFIELD:
+                return Object.assign()
+        }
+}
